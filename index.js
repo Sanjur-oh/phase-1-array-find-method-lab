d37716fe523829f21a90fc87f56cdedd2031a6c9
Michael Franco <franco.luis.michael@gmail.com>
2022-07-22 12:34:49 -0500

    commit tree 8b2547636975c7cda793af488c473557c1281a6b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// function superbowlWin (record) {
+//     if (record.result === "W") {
+//         return record.year;
+//     }
+// }
+
+// function superbowlWin(win) {
+//     let year = win.year;
+//     if (win.result === "W") {
+//       return year;
+//     } else {
+//       return undefined;
+//     }
+//   }
+  
+//   const finder = record.find(superbowlWin);
+
+function superbowlWin(record){
+  const win = record.find(elem => elem.result === "W")
+  return win ? win.year : undefined
+}
+
